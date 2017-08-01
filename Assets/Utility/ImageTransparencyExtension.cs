c1a79a3eef09f071a0b8911b2d891e222911bf1b
GodofKim <kjh.original@gmail.com>
2017-08-01 21:21:47 +0900

    Add extension codes
    - RectTransform extensions
    - Image Transparency extension

---
+++ Assets/Utility/ImageTransparencyExtension.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyExtensions
+{
+	public static class ImageTransparencyExtension
+	{
+		public static void SetTransparency(this Image p_image, float p_transparency)
+		{
+			if (p_image != null)
+			{
+				Color __alpha = p_image.color;
+				__alpha.a = p_transparency;
+				p_image.color = __alpha;
+			}
+		}
+	}
+
+}
